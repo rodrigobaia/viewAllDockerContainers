ebb44912004d04f1f75b2746b50a4064f1607056
Rodrigo Baia <rodrigo@novasistemas.com.br>
2023-03-15 23:07:48 -0300

    Configuração inicial do projeto de exibir todos os container de um servidor docker

--- ViewAllDockerContainers/ViewAllDockerContainers/Controllers/HomeController.cs
+++ ViewAllDockerContainers/ViewAllDockerContainers/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using ViewAllDockerContainers.Models;
 
@@ -7,14 +9,49 @@ namespace ViewAllDockerContainers.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        //private readonly DockerClientConfiguration _dockerConfig;
+        //private readonly DockerClient _dockerClient;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //_dockerConfig = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine"));
+            //_dockerClient = _dockerConfig.CreateClient();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //var containers = await _dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
+            //var model = new List<ContainerViewModel>();
+
+            //var dockerConfig = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine"));
+            //var dockerClient = dockerConfig.CreateClient();
+
+            //var containers = await dockerClient.Containers.ListContainersAsync(
+            //    new ContainersListParameters()
+            //    {
+            //        Limit = 10,
+            //    });
+
+            //foreach (var container in containers)
+            //{
+            //    var containerDetails = await dockerClient.Containers.InspectContainerAsync(container.ID);
+            //    var networks = string.Join(",", containerDetails.NetworkSettings.Networks.Keys);
+
+            //    model.Add(new ContainerViewModel
+            //    {
+            //        Id = container.ID,
+            //        Name = container.Names.FirstOrDefault()?.Substring(1),
+            //        Hostname = containerDetails.Config.Hostname,
+            //        InternalIP = containerDetails.NetworkSettings.Networks.Values.First().IPAddress,
+            //        ExternalIP = containerDetails.NetworkSettings.Networks.Values.First().GlobalIPv6Address,
+            //        Cpu = containerDetails.HostConfig.CPUShares,
+            //        Memory = containerDetails.HostConfig.Memory,
+            //        Networks = networks
+            //    });
+            //}
+
+            //return View(model);
             return View();
         }
 
@@ -28,5 +65,7 @@ namespace ViewAllDockerContainers.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
---
+++ ViewAllDockerContainers/ViewAllDockerContainers/Models/ContainerViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ViewAllDockerContainers.Models
+{
+    public class ContainerViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Hostname { get; set; }
+        public string InternalIP { get; set; }
+        public string ExternalIP { get; set; }
+        public long Cpu { get; set; }
+        public long Memory { get; set; }
+        public string Networks { get; set; }
+    }
+}
